cc6002c46275b9bf616dc26dd9bae09285aafede
luis-botelho <luisfellypebotelho@gmail.com>
2021-03-27 20:38:30 -0300

    add background

---
+++ app.js
@@ -0,0 +1,43 @@
+var display = document.getElementById("display")
+
+var minutos = document.getElementById("minutos")
+var segundos = document.getElementById("segundos")
+
+var começar = document.getElementById("começar")
+
+
+var cronometroSeg
+
+var minutoAtual
+var segundoAtual
+
+
+var intervalo
+
+for(var i = 0; i <= 60; i++){
+    minutos.innerHTML+='<option value="'+i+'"> '+i+'</option>';
+}
+for(var i = 1; i <= 60; i++){
+    segundos.innerHTML+='<option value="'+i+'"> '+i+'</option>';
+}
+começar.addEventListener('click',function(){
+    minutoAtual = minutos.value;
+    segundoAtual = segundos.value;
+
+    display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+    intervalo = setInterval(function(){
+        segundoAtual--;
+        if(segundoAtual <=0){
+            if(minutoAtual > 0){
+                minutoAtual--;
+                segundoAtual = 59;
+            }else{
+                alert("Acabou!");
+                document.getElementById("sound").play();
+                clearInterval(intervalo);
+            }
+        }
+
+        display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+    },1000);
+})
